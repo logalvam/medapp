504316fe5d7f90b65d6221678f75f1e9b6874005
logalvam <selvamlogan@outlook.com>
2024-02-29 07:38:33 +0530

    some changes 29/02

--- src/components/stockview.vue
+++ src/components/stockview.vue
@@ -13,32 +13,72 @@
 
       <v-data-table
         :headers="headers"
-        :items="medicine"
+        :items="newitemadd"
         :search="search"
       ></v-data-table>
     </v-card>
-    <div>{{ stock }}</div>
+    {{ newarr }}
+    <!-- <v-btn @click="click">click</v-btn> -->
   </div>
 </template>
 <script>
 export default {
+  name: "",
+  components: {},
   data() {
     return {
+      search: "",
+
       medicine: this.$store.state.medicinemaster,
       stock: this.$store.state.stock,
+      // mergedArray:this.$store.state.stock.concat(this.$store.state.medicinemaster),
       headers: [
         // {text:'Medicine Name'}, {text:'Brand Name'},{text:'Qty'},{text:'Unit Price'}
         {
           text: "Medicine Name",
           align: "start",
           sortable: false,
-          value: "name",
+          value: "medicinename",
         },
-        { text: "Brand Name", value: "calories" },
-        { text: "Qty", value: "fat" },
-        { text: "Unit Price", value: "carbs" },
+        { text: "Brand Name", value: "brandname" },
+        { text: "Qty", value: "quantity" },
+        { text: "Unit Price", value: "amount" },
       ],
+      newarr: [],
     };
+  },
+  props: {
+    // sendvalue:String
+  },
+  methods: {},
+  watch: {
+    newarr: {
+      handler() {
+        if (this.newarr.length >= 0) {
+          console.log("kkkk");
+        }
+      },
+      immediate: true,
+    },
+  },
+  computed: {
+    newitemadd() {
+      let newarr = [];
+      for (var i in this.stock) {
+        for (var j in this.medicine) {
+          if (this.stock[i].medicinename === this.medicine[j].medicinename) {
+            let mergedObject = {
+              medicinename: this.stock[i].medicinename,
+              quantity: this.stock[i].quantity,
+              amount: this.stock[i].amount,
+              brandname: this.medicine[j].brandname,
+            };
+            newarr.push(mergedObject);
+          }
+        }
+      }
+      return newarr;
+    },
   },
 };
 </script>
